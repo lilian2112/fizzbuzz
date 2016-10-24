c379347789faf4d60d6b511cbd2a3295f2802da2
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-24 15:51:00 +0100

    added divisible by 3

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -3,7 +3,11 @@ describe('Fizzbuzz', function() {
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
       fizzbuzz = new Fizzbuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(fizzbuzz.isDivisibleByThree(3)).toEqual("Fizz");
+    });
+    it('not divisible by 3', function(){
+      fizzbuzz = new Fizzbuzz();
+      expect(fizzbuzz.isDivisibleByThree(4)).toEqual(4);
     });
   });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,2 +1,9 @@
 function Fizzbuzz (){
 };
+ Fizzbuzz.prototype.isDivisibleByThree = function(number) {
+    if (number%3 == 0) {
+    return "Fizz";
+  } else {
+    return number;
+  }
+ };
