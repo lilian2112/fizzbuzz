48f566c8c257192eb03efa387a8c13886786421b
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-10-24 16:31:03 +0100

    added full functionality

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -1,25 +1,23 @@
 describe('Fizzbuzz', function() {
   var fizzbuzz;
 
-  // beforeEach(function() {
-  //   fizzbuzz = new Fizzbuzz();
-  // });
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
 
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThree(3)).toEqual("Fizz");
-    });
-
-    it('not divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByThree(4)).toEqual(4);
+      expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      fizzbuzz = new Fizzbuzz();
-      expect(fizzbuzz.isDivisibleByFive(5)).toBe("Buzz");
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
+
+    it('divisible by 3 and 5', function(){
+      expect(fizzbuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+
 
   });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,17 +1,32 @@
 function Fizzbuzz() {
 };
  Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-    if (number%3 == 0) {
-    return "Fizz";
-  } else {
-    return number;
+    if (number%3 === 0) {
+     return true;
   }
  };
 
  Fizzbuzz.prototype.isDivisibleByFive = function(number) {
-   if (number%5 == 0) {
-     return "Buzz";
-   } else {
-     return number;
+   if (number%5 === 0) {
+     return true;
    }
  };
+
+ Fizzbuzz.prototype.isDivisibleByThreeAndFive = function(number) {
+   if (number%3 === 0&&number%5 === 0) {
+     return true;
+   }
+ };
+
+Fizzbuzz.prototype.play = function(number) {
+  if (this.isDivisibleByThreeAndFive(number)) {
+    return "FizzBuzz"
+  }
+  if (this.isDivisibleByThree(number)) {
+      return "Fizz";
+  }
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz"
+  }
+  return number;
+};
